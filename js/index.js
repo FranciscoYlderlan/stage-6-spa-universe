a666a05d5e00d9c3c94c2e49e808f06a627612e0
Francisco Ylderlan <108945231+FranciscoYlderlan@users.noreply.github.com>
2022-09-16 18:00:10 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,21 @@
+import { Router } from "./router.js";
+import { menu } from "./menu.js";
+
+const router = new Router();
+
+window.route = () => router.route();
+window.onpopstate = () => router.handle();
+
+
+router.add("/","./pages/home.html");
+router.add("/home", "./pages/home.html");
+router.add("/index.html","./pages/home.html");
+router.add("/universe", "./pages/universe.html");
+router.add("/exploration","./pages/exploration.html");
+
+router.handle()
+
+
+
+
+
---
+++ js/menu.js
@@ -0,0 +1,52 @@
+class Option {
+    constructor(selector) {
+        this.isSelected = false;
+        this.option = document.querySelector(selector);
+    }
+    select() {
+        this.isSelected = true;
+        this.option.classList.add("selected");
+
+    }
+    unselect() {
+        this.isSelected = false;
+        this.option.classList.remove("selected");
+    }
+}
+
+
+export const menu = {
+    home : new Option("ul li:nth-child(1) a"),
+    universe : new Option("ul li:nth-child(2) a"),
+    exploration : new Option("ul li:nth-child(3) a"),
+
+    selected(option){
+        switch (option) {
+            case "home":
+                this.home.select();
+                this.universe.unselect();
+                this.exploration.unselect();
+            break;
+            case "universe":
+                this.home.unselect();
+                this.universe.select();
+                this.exploration.unselect();
+                break;
+            case "exploration":
+                this.home.unselect();
+                this.universe.unselect();
+                this.exploration.select();
+                break;
+        
+            default:
+                this.home.select();
+                this.universe.unselect();
+                this.exploration.unselect();
+                break;
+        }
+    }
+
+}
+
+
+

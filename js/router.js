b31a43e4e0c50621c13709229b13d25d219222ac
FranciscoYlderlan <ylderlan@nca.ufma.br>
2022-12-01 11:48:59 -0300

    fix bug content

---
+++ js/router.js
@@ -0,0 +1,40 @@
+import { menu } from "./menu.js";
+export class Router {
+    routes = {};
+
+    add (route, path) {
+        this.routes[route] = path;
+    }
+
+    route (event) {
+        event = event || window.event;
+        event.preventDefault();
+        
+        let {baseURI, href, tagName } = event.target;
+        
+        let href_ = tagName == "IMG"? baseURI.substring(0, baseURI.lastIndexOf("/")) : href;  
+        
+        window.history.pushState({},"",href_);
+        
+        this.handle();
+    }
+
+    handle () {
+
+        let {pathname} = window.location;
+
+        let route = this.routes[pathname] 
+        
+        let option = pathname.replace("/","");
+        
+        menu.selected(option)
+        
+        fetch(route)
+        .then(page => page.text())
+        .then(html => 
+        { 
+            document.querySelector("#app").innerHTML = html;
+        })
+    
+    }
+}
